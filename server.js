dff4496b75d827723e08c94331bce9d319303cf8
icepants <shawnluther99@gmail.com>
2019-07-24 00:46:57 -0400

    Created the basic server for our express app.

--- server.js
+++ server.js
@@ -2,3 +2,19 @@
 const express = require("express");
 const path = require("path");
 
+// Define new express app instance.
+const app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+
+
+
+
+// Start Server.
+app.listen(PORT, function () {
+    // Let's us know the server is running in the console.
+    console.log("Server listening on: http://localhost:" + PORT);
+});
